63b8fed95d5d6d4287b747693c81335df7c52e13
Only Wan <lazuardojuan@gmail.com>
2020-05-09 21:44:37 +0700

    Membuat Splash Screen

---
+++ app/src/main/java/com/apps/juanlazuardo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.apps.juanlazuardo;
+    /*
+
+    NIM     : 10117160
+    NAMA    : JUAN LAZUARDO
+    KELAS   : AKB-4 / IF-4
+    Update  :
+        9 Mei 2020 : - Membuat Splash Screen
+                     - Merubah Icon Aplikasi
+
+    */
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
